f7461e67d570c7b882bc27ec46e955d9f07d765e
Tarnowka <48628472+Tarnowka@users.noreply.github.com>
2019-10-07 14:15:25 +0200

    Update app.js

---
+++ 1_Zadania/obowiązkowe/2_Tablice/js/app.js
@@ -0,0 +1,27 @@
+function distFromAvarage(numberArray){
+    var avarage=0;
+    numberArray.forEach(element => {
+        avarage+=element;
+    });
+
+    avarage=avarage/numberArray.length;
+    console.log(avarage);
+    var returnArray=[];
+    numberArray.forEach(element => {
+        returnArray.push(element-avarage);
+    });
+
+    console.log(returnArray);
+    return returnArray;
+}
+
+distFromAvarage([1,2,3,4,5,6,7]);
+
+////////////////////////////////////
+
+let fruits = ['apple', 'orange', 'banana', 'peach', 'pear'];
+console.log(fruits[0]);
+console.log(fruits[fruits.length-1]);
+for(let count=0;count<fruits.length;count++){
+    console.log(fruits[count]);
+}
