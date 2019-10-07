8523511eb38da4f4d90123b9c1e3e1112618c860
Tarnowka <48628472+Tarnowka@users.noreply.github.com>
2019-10-07 14:01:40 +0200

    Update app.js

---
+++ 1_Zadania/obowiązkowe/1_Funkcje_czasu/js/app.js
@@ -0,0 +1,11 @@
+function countHello(number){
+    var count=0;
+    var intervalid=setInterval(()=>{
+        count++;
+        console.log('Hello', count);
+        if(count===number){
+            clearInterval(intervalid);
+        }
+    },500);
+}
+countHello(5);
