5fbb1bb0d089a66325bb58a8ee0c930f6f3c9ed3
Tarnowka <48628472+Tarnowka@users.noreply.github.com>
2020-01-11 21:18:14 +0100

    11

---
+++ 1_Zadania/obowiązkowe/11_DOM_Eventy/js/zadanie00.js
@@ -0,0 +1,11 @@
+const childrens = Array.from(document.querySelectorAll('.children'))
+
+childrens.forEach(element => {
+  element.parentNode.addEventListener('mouseover', function() {
+    element.style = 'display: block'
+  })
+
+  element.parentNode.addEventListener('mouseleave', function() {
+    element.style = ''
+  })
+})
---
+++ 1_Zadania/obowiązkowe/11_DOM_Eventy/js/zadanie02.js
@@ -0,0 +1,5 @@
+window.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('mainBtn').addEventListener('click', () => {
+      console.log('Hura! Działa!')
+    })
+  })
